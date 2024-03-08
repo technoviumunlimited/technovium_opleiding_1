dc921b75940ea69db99a9d3273413039168d355f
davor <dradic@roc-nijmegen.nl>
2024-03-08 15:35:16 +0100

    main

--- main.gd
+++ main.gd
@@ -1,3 +1,11 @@
+# TODO
+# uitroepteken add audio
+# logo van shielder in lift
+# in de beeld alle opleidingen als je het had bezocht wordt het rood soor t check point
+# lift automatisch naar beneden DONE 
+# lift area, call is beter dan na 20 sec
+# collision met bijgebouw 
+
 extends Node3D
 
 # Called when the node enters the scene tree for the first time.
